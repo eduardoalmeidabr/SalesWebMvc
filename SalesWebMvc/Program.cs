4542a21f121816538b0e2fb626782fde55c05853
Eduardo Almeida <eduardo.ds.almeida@gmail.com>
2024-08-24 14:59:18 -0300

    Seller Service and Basic FindAll

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 );
 
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
