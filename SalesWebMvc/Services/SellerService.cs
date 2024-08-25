01620e259108226fd8e4f549011f0c4352522c76
Eduardo Almeida <eduardo.ds.almeida@gmail.com>
2024-08-25 12:08:01 -0300

    Tela de detalhes e eager loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -26,7 +26,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(s => s.Id == id);
+            return _context.Seller.Include(s => s.Department).FirstOrDefault(s => s.Id == id);
         }
 
         public void Remove(int id)
